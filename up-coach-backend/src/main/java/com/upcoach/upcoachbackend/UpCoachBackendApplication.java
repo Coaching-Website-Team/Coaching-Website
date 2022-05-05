02c9bcdc99ae6d24537d299495d4007058d97b98
HamzaBenyazid <benyzidhamza969@gmail.com>
2022-05-05 13:48:34 +0000

    initialize backend

---
+++ up-coach-backend/src/main/java/com/upcoach/upcoachbackend/UpCoachBackendApplication.java
@@ -0,0 +1,13 @@
+package com.upcoach.upcoachbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpCoachBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpCoachBackendApplication.class, args);
+	}
+
+}
---
+++ up-coach-backend/src/test/java/com/upcoach/upcoachbackend/UpCoachBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upcoach.upcoachbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpCoachBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
