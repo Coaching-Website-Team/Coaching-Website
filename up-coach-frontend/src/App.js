a82b1636577c08638e92d223271b951919b3642b
HamzaBenyazid <benyzidhamza969@gmail.com>
2022-05-05 13:13:44 +0000

    initialize frontend and create NavBarUnauthenticated component

---
+++ up-coach-frontend/src/App.js
@@ -0,0 +1,50 @@
+import Navbar from "./components/NavbarUnauthenticated/NavbarUnauthenticated";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import "@fontsource/space-grotesk"; // Defaults to weight 400.
+import backgroundImage from "./res/images/background_image.jpg";
+
+const styles = {
+  appBody: {
+    minHeight: "100vh",
+    backgroundColor: "rgba(255, 255, 255,0.5)",
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundPosition: "top",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    boxShadow: "inset 0 0 0 1000px rgba(255,255,255,.5)",
+  },
+};
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Space Grotesk"].join(","),
+    fontSize: 15,
+    button: {
+      textTransform: "none",
+    },
+  },
+  palette: {
+    primary: {
+      main: "#F58025",
+    },
+    secondary: {
+      main: "#6495ED",
+    },
+    grey: {
+      main: "#8C92AC",
+    },
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Navbar />
+        <div style={styles.appBody}></div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
