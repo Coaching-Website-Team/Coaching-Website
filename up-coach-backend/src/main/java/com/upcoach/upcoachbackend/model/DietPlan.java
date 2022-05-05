8d263cbd2a07cf166fb03eade3af577b6d84f5ce
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-05-05 18:05:59 +0000

    added diet plan entity -- UP-32 --

---
+++ up-coach-backend/src/main/java/com/upcoach/upcoachbackend/model/DietPlan.java
@@ -0,0 +1,53 @@
+package com.upcoach.upcoachbackend.model;
+
+import org.hibernate.Hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class DietPlan {
+
+
+    @Column(name = "plan_location", nullable = false, length = 500)
+    private String planLocation;
+
+
+
+    public DietPlan(String planLocation) {
+        this.planLocation = planLocation;
+    }
+
+    public DietPlan() {
+
+    }
+
+
+    public String getPlanLocation() {
+        return planLocation;
+    }
+
+    public void setPlanLocation(String planLocation) {
+        this.planLocation = planLocation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        DietPlan dietPlan = (DietPlan) o;
+        return planLocation != null && Objects.equals(planLocation, dietPlan.planLocation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(planLocation);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "planLocation = " + planLocation + ")";
+    }
+}
