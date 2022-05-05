ac475c941d04dcbedf29b1949972bf9adbf9811b
Amina Chaabane <amina.chaabane34@gmail.com>
2022-05-05 21:10:48 +0000

    add OfferRepository

---
+++ up-coach-backend/src/main/java/com/upcoach/upcoachbackend/repository/OfferRepository.java
@@ -0,0 +1,17 @@
+package com.upcoach.upcoachbackend.repository;
+
+import com.upcoach.upcoachbackend.model.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+    Optional<List<Offer>> findAllByTagsIn(List<String> tags);
+
+    Optional<List<Offer>> findAllByCoachId(long id);
+
+    Optional<List<Offer>> findAllOffersByCoachFirstNameAndCoachLastName(String firstName, String lastName);
+
+    Optional<List<Offer>> findAllByTitle(String title);
+}
