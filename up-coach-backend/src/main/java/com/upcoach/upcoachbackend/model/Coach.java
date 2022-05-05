f0a00c9891608db58dbb8401c5adfd5a97f50270
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-05-05 19:53:45 +0000

    added coach model -- UP-13 -- UP-50

---
+++ up-coach-backend/src/main/java/com/upcoach/upcoachbackend/model/Coach.java
@@ -0,0 +1,108 @@
+package com.upcoach.upcoachbackend.model;
+
+import com.upcoach.upcoachbackend.enums.Level;
+import com.upcoach.upcoachbackend.enums.Role;
+import com.upcoach.upcoachbackend.enums.Speciality;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Coach extends User{
+
+    private boolean verified = false;
+    @ElementCollection(targetClass = Speciality.class)
+    @JoinTable(name = "specialities", joinColumns = @JoinColumn(name = "ID"))
+    @Column(name = "specialities")
+    @NotNull(message = "Speciality is required")
+    @Enumerated(EnumType.STRING)
+    private List<Speciality> specialities;
+
+    @Enumerated(EnumType.STRING)
+    private Level level;
+
+    /**
+     * path to id card
+     */
+    private String identityDocument;
+    /**
+     * path to coaching certificate
+     */
+    private String coachingCertificate ;
+    /**
+     * path to image of coach
+     */
+    private String image;
+
+    public Coach(String firstName, String lastName, Date birth_date, Role role, String email, String password, boolean verified, List<Speciality> specialities, Level level, String identityDocument, String coachingCertificate, String image) {
+        super(firstName, lastName, birth_date, role, email, password);
+        this.verified = verified;
+        this.specialities = specialities;
+        this.level = level;
+        this.identityDocument = identityDocument;
+        this.coachingCertificate = coachingCertificate;
+        this.image = image;
+    }
+
+    public String getIdentityDocument() {
+        return identityDocument;
+    }
+
+    public void setIdentityDocument(String identityDocument) {
+        this.identityDocument = identityDocument;
+    }
+
+    public String getCoachingCertificate() {
+        return coachingCertificate;
+    }
+
+    public void setCoachingCertificate(String coachingCertificate) {
+        this.coachingCertificate = coachingCertificate;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Coach(String first_name, String last_name, Date birth_date,
+                 String email, String password,
+                 boolean verified, List<Speciality> specialities, Level level) {
+        super(first_name, last_name, birth_date, Role.COACH, email, password);
+        this.verified = verified;
+        this.specialities = specialities;
+        this.level = level;
+    }
+
+    public Coach() {}
+
+
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
+
+    public List<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(List<Speciality> specialities) {
+        this.specialities = specialities;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+}
