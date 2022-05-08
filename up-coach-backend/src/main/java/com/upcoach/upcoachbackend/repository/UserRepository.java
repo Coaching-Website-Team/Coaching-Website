305905e816520e9ba71d287ecf92964973c5c45f
Amina Chaabane <amina.chaabane34@gmail.com>
2022-05-08 22:20:09 +0100

    update UserRepository

--- up-coach-backend/src/main/java/com/upcoach/upcoachbackend/repository/UserRepository.java
+++ up-coach-backend/src/main/java/com/upcoach/upcoachbackend/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.upcoach.upcoachbackend.repository;
 import com.upcoach.upcoachbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository<T extends User> extends JpaRepository<T, Long> {
     boolean existsByEmail(String email);
+    Optional<T> findByEmail(String email);
 }
