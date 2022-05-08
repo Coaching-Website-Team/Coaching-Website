76e5ebd923f215cb5a56966e70d500c9cc87cbe9
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-05-08 16:24:27 +0100

    user already exists exception -- UP-75 --

---
+++ up-coach-backend/src/main/java/com/upcoach/upcoachbackend/exception/UserAlreadyExistsException.java
@@ -0,0 +1,13 @@
+package com.upcoach.upcoachbackend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class UserAlreadyExistsException extends RuntimeException{
+
+    public UserAlreadyExistsException(String email) {
+        super("User with email " + email + " already exits!");
+    }
+}
+
