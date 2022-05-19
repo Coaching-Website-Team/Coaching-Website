d10b6de0c95ae53e7ceca721494aa8b7463e9a21
HamzaBenyazid <benyzidhamza969@gmail.com>
2022-05-19 17:21:23 +0100

    create security config class (UP-34)

---
+++ up-coach-backend/src/main/java/com/upcoach/upcoachbackend/service/UserPrincipalDetailService.java
@@ -0,0 +1,32 @@
+package com.upcoach.upcoachbackend.service;
+
+import com.upcoach.upcoachbackend.model.User;
+import com.upcoach.upcoachbackend.security.UserPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional
+
+public class UserPrincipalDetailService implements UserDetailsService {
+    private UserService userService;
+
+    public UserPrincipalDetailService(UserService userService){
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email){
+        User user = null;
+        try {
+            user = userService.getUserWithEmail(email);
+            user.setActive(true);
+        } catch (Throwable e) {}
+        if (user == null) throw new UsernameNotFoundException(email);
+        return new UserPrincipal(user);
+    }
+}
