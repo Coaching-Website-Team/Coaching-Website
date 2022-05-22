c13c1b11ff1d4bbfb1c99eb56edb1b2bdd0d61ef
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-05-22 22:37:33 +0100

    working on client dashboard

---
+++ up-coach-frontend/src/components/Dashboard/Client/Body.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import Img, { QuoteImage } from "./Img";
+import Title from "./Title";
+import Description from "./Description";
+import { TrainingSessions, MealPlans } from "./Buttons";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import { makeStyles } from "@mui/styles";
+import Search from "./Search";
+import AlLCards from "./Trainers/AlLCards";
+import ChooseOffer from "./ChooseOffer";
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+    position: "relative",
+
+    backgroundColor: "transparent",
+    opacity: 1
+  },
+  paper: {
+    padding: 0,
+    textAlign: "left",
+    color: "theme.palette.text.secondary",
+    backgroundColor: "transparent",
+    boxShadow: "0 0 0 0 transparent",
+    position: "absolute",
+    alignItems: "center",
+
+  },
+  list: {
+
+    listStyle: "none",
+    backgroundColor: "transparent",
+  },
+  li: {
+    marginTop: "50px",
+    // position:"absolute",
+
+    backgroundColor: "transparent",
+  },
+  liButton: {
+    marginTop: "100px",
+
+    backgroundColor: "transparent",
+  },
+  lii: {
+    marginTop: "40%",
+    marginLeft: "10%",
+
+    "& > *": {
+      fontFamily: "Montserrat",
+      fontWeight: 400,
+
+      backgroundColor: "transparent",
+    },
+  },
+  course: {
+    marginTop: "4%",
+    marginLeft: "5%",
+
+    backgroundColor: "transparent",
+  },
+  marginAll: {
+    marginLeft: "5%",
+    marginTop: "4%",
+
+  }
+});
+function Body(props) {
+  let style = useStyles();
+
+  return (
+    <div style={props.styles}>
+
+      <div className={style.root} onMouseOver={props.onMouseOver}>
+        <ol className={style.list}>
+          <li className={style.li}>
+            <Grid container spacing={1} justify="space-around" style={{ backgroundColor: "transparent" }}>
+              <Grid item xs={6} className={style.marginAll}>
+                <Paper className={style.paper}>
+                  <li className={style.li} >
+                    <Title title=" Welcome back  !" />
+
+
+                  </li>
+                  <ol className={style.list}>
+                    <ChooseOffer />
+                    <li className={style.li}>
+                      < AlLCards />
+
+
+                    </li>
+
+                  </ol>
+                </Paper>
+              </Grid>
+              <Grid item xs={5} style={{ marginTop: "5%", position: "absolute", right: "30%" }}>
+
+              </Grid>
+            </Grid>
+          </li>
+        </ol>
+      </div>
+
+    </div>
+  );
+}
+export default Body;
---
+++ up-coach-frontend/src/components/Dashboard/Client/Trainers/TrainerCard.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import hajar_photo from "../../../../css/coach.jpg";
+import styled from "styled-components";
+import TextButton from "../../../shared/TextButton"
+function TrainerCard(props) {
+  const Container = styled.div`
+ width: 100%;
+  max-width: 80%;
+  
+  background:#FFFFFF;
+  padding: 44px;
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
+  text-align: left;
+  border-radius: 4px;
+  
+ 
+`;
+
+  const Img = styled.img`
+display: block;
+  margin-left: auto;
+  margin-right: auto;
+ height: 100px;
+ width: 150px;
+  
+
+
+
+`;
+
+  const Test1 = styled.h5`
+color: #ed563b;
+margin: 26px 0 9px 0;
+font-weight: 500;
+font-size: 14px
+ 
+`;
+
+  const Test2 = styled.h3`
+color: rgba(15, 103, 8, 1);
+letter-spacing: 1px;
+fontFamily: "Rubik Beastly";
+  }
+`;
+
+  const Test3 = styled.p`
+margin: 24px 0 28px 0;
+    color: #7a7a7a;
+    font-size: 15px;
+    font-weight: 400;
+    line-height: 1.7;
+    
+overflow: hidden;
+
+display: -webkit-box;
+-webkit-line-clamp: 3;
+-webkit-box-orient: vertical;
+  
+
+    
+
+`;
+  return (
+    <Container>
+      <Test2>{props.name}</Test2>
+      <Img src={props.image} alt="trainer" />
+      <Test3>{props.desc}</Test3>
+      <Test1>{props.price}</Test1>
+
+      < TextButton value="Details" fgColor="rgba(15, 103, 8, 1)" url={props.url}> </TextButton>
+
+    </Container>);
+}
+
+
+export default TrainerCard;
+
